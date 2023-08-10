060db58b52a3e2eaed7842b6742ae077c5efb4d6
Derek <dhamltonstl@gmail.com>
2023-08-10 12:47:09 -0500

    add spots router

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { Spot } = require('../../db/models');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => )
+
+
+
+
+
+module.exports = router;
